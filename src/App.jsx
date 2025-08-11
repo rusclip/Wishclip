8929dae9832efadfdc4b8a6fb5e3e705d2cf87c3
rusclip <istinavnytri@gmail.com>
2025-08-11 22:12:06 +0600

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ padding: '40px', textAlign: 'center', fontFamily: 'Arial' }}>
+      <h1 style={{ color: 'blue' }}>🎉 ПОЗДРАВЛЯЮ!</h1>
+      <p>Этот сайт работает!</p>
+    </div>
+  );
+}
