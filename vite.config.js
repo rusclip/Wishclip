13e7048fe73e1e8a2e17c9ecebc150bcbf91f87c
rusclip <istinavnytri@gmail.com>
2025-08-12 00:51:29 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: 'public/index.html'  // 🔥 Обязательно!
-    }
-  },
-  root: '.'  // Явно указываем корень проекта
+  plugins: [react()]
 });
+
