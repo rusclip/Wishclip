fd290d4cd13da6a2095b8328217558ad5d5a6921
rusclip <istinavnytri@gmail.com>
2025-08-20 21:08:32 +0500

    Create step1.js

---
+++ pages/step1.js
@@ -0,0 +1,8 @@
+export default function Step1() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Step 1: Upload your photo</h1>
+      <p>Here the user will upload their photo or choose a template.</p>
+    </div>
+  );
+}
