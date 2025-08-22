5b4be69d7069e5148401149cdc10513878cb4346
rusclip <istinavnytri@gmail.com>
2025-08-22 09:51:41 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   build: {
     rollupOptions: {
-      input: {
-        main: 'index.html',
-        chat: 'chat.html'
-      }
+      input: 'index.html'
     }
   }
-})
+});
