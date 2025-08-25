9d307d97153c5be2f290980611531facfb34c645
rusclip <istinavnytri@gmail.com>
2025-08-25 18:15:37 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-import "./styles.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import QR from "./pages/QR.jsx";
+import Chat from "./pages/Chat.jsx";
+import "./App.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/qr" element={<QR />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
