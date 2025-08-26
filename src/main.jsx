44fa96e5973e401f7f70a87bf115233a58306a2a
rusclip <istinavnytri@gmail.com>
2025-08-26 20:56:12 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css"; // общий CSS
+import App from "./App.jsx";
+import "./styles.css";   // ✅ используем только styles.css
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
